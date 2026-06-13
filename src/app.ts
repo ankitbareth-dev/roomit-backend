710246868f281178175aaff896635bcfdf2ea9f7
Ankit Bareth <ankitbareth.dev@gmail.com>
2026-06-13 23:09:47 +0530

    feat: configure router for get room and register in the app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application } from "express";
 import morgan from "morgan";
 import { globalErrorHandler } from "./middlewares/globalErrorHandler.js";
+import roomRoutes from "./routes/room.routes.js";
 
 const app: Application = express();
 app.use(morgan("dev"));
@@ -10,6 +11,7 @@ app.get("/", (_req, res) => {
     message: "Server is running",
   });
 });
+app.use("/api/rooms", roomRoutes);
 app.use(globalErrorHandler);
 
 export default app;
